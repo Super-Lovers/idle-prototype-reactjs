c49e405ab3f88c8de2337747e7c66f567b2219da
Nikolay Ivanov <n.v.ivanov@st.hanze.nl>
2021-01-21 22:20:54 +0100

    Designed tiers and upgrades for items

--- src/upgrades/upgrades_seed copy.js
+++ src/upgrades/upgrades_seed copy.js
@@ -5,110 +5,230 @@ const upgrades = {
 			icon: '☕',
 			color: 'brown',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Green Tea',
 			icon: '🍵',
 			color: 'olive',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Potato Chips',
 			icon: '🍟',
 			color: 'orange',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'All-nighter',
 			icon: '🌙',
 			color: 'blue',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Mountain Dew',
 			icon: '🧃',
 			color: 'green',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Chocolate',
 			icon: '🍫',
 			color: 'brown',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Coke',
 			icon: '🥤',
 			color: 'red',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Buiscuits',
 			icon: '🍪',
 			color: 'brown',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Takeaway KFC',
 			icon: '🐔',
 			color: 'red',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 		{
 			title: 'Mom\'s Mashed Potatoes',
 			icon: '🥔',
 			color: 'pink',
 			requirements: {
-				'lines_of_code': 1000,
+				lines_of_code: 1000,
 			},
 			unlocked: false,
 			visible: true,
 			quantity: 0,
+			price_properties: {
+				multiplier: 1.15,
+				base_price: 100,
+				current_price: 115
+			},
+			output_properties: {
+				multiplier: 1.1,
+				base_increment: 0.2,
+				base_output: 0.22,
+				current_increment: 0.0484,
+				current_multiplier: 0.044
+			}
 		},
 	]
 }
