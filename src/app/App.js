5a31f59ebd4b71d81b57e706aea41a7e8025987f
Nikolay Ivanov <n.v.ivanov@st.hanze.nl>
2021-01-22 10:58:03 +0100

    Added counter + clicking to programmer

--- src/app/App.js
+++ src/app/App.js
@@ -12,13 +12,16 @@ import Upgrades from '../upgrades/Upgrades';
 class App extends React.Component {
 	state = {
 		'characters_of_code': 0,
-		'lines_of_code': 2000,
+		'lines_of_code': 100,
 	};
 
 	render() { 
 		return (
 			<div className='app ui vertically divided grid container'>
-				<Programmer />
+				<Programmer
+					lines_of_code={this.state.lines_of_code}
+					handleClickIncrementCode={this.clickIncrementCode}
+				/>
 				<Milestones />
 				<Upgrades
 					data={this.state}
@@ -26,6 +29,18 @@ class App extends React.Component {
 			</div>
 		);
 	};
+
+	// Triggered on tap/click
+	clickIncrementCode = () => {
+		this.incrementLinesOfCode(1);
+	}
+
+	// Triggered automatically periodically or through a tap/click
+	incrementLinesOfCode = (increment) => {
+		this.setState((previousState, props) => ({
+			lines_of_code: previousState.lines_of_code + increment
+		}));
+	}
 }
 
 export default App;
--- src/programmer/Programmer.js
+++ src/programmer/Programmer.js
@@ -9,11 +9,29 @@ class Programmer extends React.Component {
 		return (
 			<div className='programmer row ui vertically divided grid'>
 				<h2>Programmer</h2>
-				<div className='ui row'>
-					<img className='ui centered rounded image programmer' src='bango_programmer.gif' alt='programmer cat' />
+				<div className='ui row one column'>
+					<div className='ui column centered lines_of_code'>
+						<span className='lines_of_code_number'>
+						<br/>
+							{this.props.lines_of_code}
+						</span>
+						<p className='lines_of_code_label'> lines of code</p>
+					</div>
+					<div className='ui column'>
+						<img 
+							className='ui centered rounded image programmer'
+							src='bango_programmer.gif'
+							alt='programmer cat'
+							onClick={this.handleClick}	
+						/>
+					</div>
 				</div>
 			</div>
 		);
+	};
+
+	handleClick = () => {
+		this.props.handleClickIncrementCode();
 	};
 }
 
